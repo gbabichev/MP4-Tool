9f692ff8b15ea96cbe43faa6f5a8c9d4cd6bd51a
George Babichev <gbabichev@xantrion.com>
2025-10-11 18:16:40 -0400

    Initial Commit

---
+++ MP4 Tool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MP4 Tool
+//
+//  Created by George Babichev on 10/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MP4 Tool/MP4_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MP4_ToolApp.swift
+//  MP4 Tool
+//
+//  Created by George Babichev on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MP4_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
